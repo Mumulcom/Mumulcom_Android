0d2734b6844131ace80f0e4b72ebbce0941011e7
Jiwon116 <ji_1106@naver.com>
2022-02-20 23:40:08 +0900

    update profile(22.02.20)

--- app/src/main/java/com/example/mumulcom/ProfileFragment.kt
+++ app/src/main/java/com/example/mumulcom/ProfileFragment.kt
@@ -16,10 +16,7 @@ class ProfileFragment : Fragment(), ProfileView {
     private var jwt: String = ""    // jwt
     private var userIdx: Long = 0   // 유저 인덱스
     private var email: String = ""  // 유저 이메일
-    private var name: String = ""   // 유저 이름
     private var nickname: String = ""   // 유저 닉네임
-    private var group: String = ""  // 유저 소속
-    private var myCategories: MutableList<String> = mutableListOf() // 유저 관심 카테고리
     private var profileImgUrl: String = ""  // 유저 프로필 이미지
 
 
@@ -30,21 +27,45 @@ class ProfileFragment : Fragment(), ProfileView {
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        // SharedPreference에 저장한 정보 불러오기
         jwt = context?.let { getJwt(it) }.toString()
         userIdx = context?.let { getUserIdx(it) }!!
+        email = context?.let { getEmail(it) }.toString()
+        nickname = context?.let { getNickname(it) }.toString()
+        profileImgUrl = context?.let { getProfileImgUrl(it) }.toString()
+        // name = context?.let { getName(it) }.toString()
+        // group = context?.let { getGroup(it) }.toString()
+        // myCategories = context?.let { getCategories(it) }!!
+
+        initView()
 
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
-        getProfile(jwt, userIdx)
-    }
-
     private fun initView() {
-        context?.let { Glide.with(it).load(profileImgUrl).into(binding.profileProfileIv) }
         binding.profileNicknameTv.text = nickname
-        binding.profileGroupTv.text = group
+        binding.profileEmailTv.text = email
+
+        when {
+            profileImgUrl.contains("1.png") -> {
+                binding.profileBackgroundLy.setBackgroundResource(R.drawable.ic_profile_bg_1)
+            }
+            profileImgUrl.contains("2.png") -> {
+                binding.profileBackgroundLy.setBackgroundResource(R.drawable.ic_profile_bg_2)
+            }
+            profileImgUrl.contains("3.png") -> {
+                binding.profileBackgroundLy.setBackgroundResource(R.drawable.ic_profile_bg_3)
+            }
+            profileImgUrl.contains("4.png") -> {
+                binding.profileBackgroundLy.setBackgroundResource(R.drawable.ic_profile_bg_4)
+            }
+            profileImgUrl.contains("5.png") -> {
+                binding.profileBackgroundLy.setBackgroundResource(R.drawable.ic_profile_bg_5)
+            }
+            profileImgUrl.contains("6.png") -> {
+                binding.profileBackgroundLy.setBackgroundResource(R.drawable.ic_profile_bg_6)
+            }
+        }
     }
 
     private fun getProfile(jwt: String, userIdx: Long){
@@ -61,15 +82,7 @@ class ProfileFragment : Fragment(), ProfileView {
     override fun onGetProfileSuccess(result: Profile) {
         Log.d("Profile/API","성공")
         if (result != null) {   // 회원 정보가 있으면
-            // SharedPreference에 저장한 정보 불러오기
-            email = context?.let { getEmail(it) }.toString()
-            name = context?.let { getName(it) }.toString()
-            nickname = context?.let { getNickname(it) }.toString()
-            group = context?.let { getGroup(it) }.toString()
-            myCategories = context?.let { getCategories(it) }!!
-            profileImgUrl = context?.let { getProfileImgUrl(it) }.toString()
-
-            initView()
+            //initView()
         }
     }
 
